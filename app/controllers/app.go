88af162fe40baaf0bd1e85ee0292d2b7efbaed87
orps <0rps>
2016-04-20 00:07:27 +0300

    [upd] authorization package

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,7 +1,10 @@
 package controllers
 
-import "github.com/revel/revel"
-import "github.com/0rps/goapp/app/models"
+import (
+	"github.com/0rps/goapp/app/models"
+	"github.com/revel/revel"
+	"strconv"
+)
 
 type App struct {
 	*revel.Controller
@@ -11,8 +14,10 @@ func (c App) Login(login, password string) revel.Result {
 	revel.INFO.Printf("login is '%s', pass is '%s'", login, password)
 
 	if login != "" || password != "" {
-		_, noerror := models.Authorize(login, password)
+		session, noerror := models.Authorize(login, password)
 		if noerror {
+			c.Session["session_id"] = strconv.FormatInt(session.Id, 10)
+			c.Session["session_secret"] = session.Secret
 			c.Flash.Success("Пользователь авторизован")
 			return c.Redirect("/")
 		} else {
@@ -30,7 +35,7 @@ func (c App) Register(login, password, repassword string) revel.Result {
 	if login != "" || password != "" || repassword != "" {
 		c.Validation.Required(login).Message("Введите логин")
 		c.Validation.Required(password).Message("Введите пароль")
-		c.Validation.MinSize(password, 4).Message("Минимальная длина пароля - 8 символов")
+		c.Validation.MinSize(password, 4).Message("Минимальная длина пароля - 4 символа")
 
 		if c.Validation.HasErrors() {
 			c.Validation.Keep()
@@ -43,15 +48,17 @@ func (c App) Register(login, password, repassword string) revel.Result {
 			return c.Redirect("/register")
 		}
 
-		if models.IsUserExists(login) {
+		if _, err := models.FindUserByLogin(login); err == nil {
 			c.Flash.Error("Пользователь с таким именем существует")
 			return c.Redirect("/register")
 		}
 
-		user := models.CreateUser(login, password)
-		if user.Id > 0 {
+		user, err := models.CreateUser(login, password)
+		if err == nil {
 			c.Flash.Success("Пользователь создан, id = ", user.Id)
 			return c.Redirect("/")
+		} else {
+			revel.INFO.Println(err)
 		}
 	}
 
--- app/models/models.go
+++ app/models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/0rps/goapp/app"
 	"github.com/revel/revel"
@@ -18,90 +20,163 @@ type Session struct {
 	UserId int
 }
 
-func CreateUser(login, pass string) User {
+func CreateUser(login, pass string) (User, error) {
 	var user User
-
-	queryRes, err := app.DB.Exec(`
+	var err error
+	queryRes, queryErr := app.DB.Exec(`
 		INSERT INTO Users(login, password)
 		VALUES(?,?)`, login, pass)
 
-	if err != nil {
-		revel.INFO.Print("Error in createuser: ", err)
+	if queryErr != nil {
+		revel.INFO.Print("Error in createuser: ", queryErr)
+		err = errors.New("DB error")
 	} else {
 		var id int64
-		id, err = queryRes.LastInsertId()
+		id, _ = queryRes.LastInsertId()
 		user = User{Login: login, Password: pass, Id: int(id)}
 	}
-	return user
+
+	return user, err
 }
 
-func FindUser(id int) (User, bool) {
-	var user User
-	noerror := false
-	if id == 1 {
-		user = User{Login: "god", Password: "1234", Id: 1}
-		noerror = true
+func FindUserById(id int) (User, error) {
+	var (
+		user     User
+		err      error
+		login    string
+		password string
+	)
+
+	queryErr := app.DB.QueryRow(`
+		SELECT login, password FROM Users 
+		WHERE id=?`, id).Scan(&login, &password)
+
+	switch {
+	case queryErr == sql.ErrNoRows:
+		err = errors.New("No such user")
+	case err != nil:
+		err = queryErr
+	default:
+		user = User{Id: id, Login: login, Password: password}
 	}
-	return user, noerror
+
+	return user, err
 }
 
-func FindUser(login string) bool {
-	// queryStr := fmt.Sprintf("SELECT COUNT(*) AS cnt FROM Users WHERE login like '%%s%'", login)
-	// /// TODO: fix
-	// queryRes, err := app.DB.Query(queryStr)
+func FindUserByLogin(login string) (User, error) {
+	var (
+		user     User
+		err      error
+		id       int
+		password string
+	)
 
-	return false
-}
+	queryErr := app.DB.QueryRow(`
+		SELECT id, password FROM Users 
+		WHERE login LIKE ?`, login).Scan(&id, &password)
 
-func FindUser(login, password string) (User, bool) {
-	return User{}, false
+	switch {
+	case queryErr == sql.ErrNoRows:
+		err = errors.New("No such user")
+	case err != nil:
+		err = queryErr
+	default:
+		user = User{Id: id, Login: login, Password: password}
+	}
+
+	return user, err
 }
 
 func Authorize(login, pass string) (Session, bool) {
 	var session Session
-	var err bool
-	// user, isExists :=FindUser(login, pass)
-	// if isExists {
-	// 	session, err = createSession(user.Id)
-	// } else {
-	// 	return session, false
-	// }
+
+	user, err := FindUserByLogin(login)
+	if err == nil {
+
+		if user.Password == pass {
+			session, err = createSession(user.Id)
+			if err == nil {
+				return session, true
+			} else {
+				revel.INFO.Println("cannot authorize, cannot create session, err: ", err)
+			}
+		} else {
+			revel.INFO.Printf("cannot authorize, diff passwords: %s and %s", pass, user.Password)
+		}
+	} else {
+		revel.INFO.Printf("cannot authorize, no such user with login: %s, err = %s", login, err.Error())
+	}
+
 	return session, false
 }
 
-func GetSession(id int, secret string) (Session, bool) {
-	var session Session
+func GetSession(id int, secret string) (Session, error) {
+	var (
+		session Session
+		err     error
+	)
 
-	return session, false
+	queryErr := app.DB.QueryRow(`
+		SELECT secret, userId FROM Sessions 
+		WHERE id=?`, id).Scan(&session.Secret, &session.UserId)
+
+	switch {
+	case queryErr == sql.ErrNoRows:
+		err = errors.New(fmt.Sprintf("No such session with id: %d", id))
+	case err != nil:
+		err = queryErr
+	default:
+		if session.Secret == secret {
+			session.Id = id
+		} else {
+			session = Session{}
+			err = errors.New(fmt.Sprintf("Wrong secret for session with id: %d", id))
+		}
+	}
+
+	return session, err
 }
 
 func generateSecret() string {
 	return "12345667"
 }
 
-func createSession(userId int) (Session, bool) {
+func createSession(userId int) (Session, error) {
 	var session Session
-	// secret := generateSecret()
-	// queryRes, err := app.DB.Exec(`
-	// 	INSERT INTO Sessions(secret, user_id)
-	// 	VALUES(?,?)`, secret, userId)
+	var err error
 
-	// if err != nil {
-	// 	revel.INFO.Print("Error in createSession: ", err)
-	// } else {
-	// 	var id int64
-	// 	id, err = queryRes.LastInsertId()
-	// 	user = Session{Secret: secret, UserId: userId, Id: int(id)}
-	// 	return session, true
-	// }
+	secret := generateSecret()
+	queryRes, queryErr := app.DB.Exec(`
+		INSERT INTO Sessions(secret, user_id)
+		VALUES(?,?)`, secret, userId)
 
-	return session, false
+	if queryErr != nil {
+		revel.INFO.Print("Error in createSesson: ", queryErr)
+		err = errors.New("DB error")
+	} else {
+		var id int64
+		id, _ = queryRes.LastInsertId()
+		session = Session{Id: int(id), Secret: secret, UserId: userId}
+	}
+
+	return session, err
 }
 
-func (s *Session) GetUser() (User, bool) {
-	return FindUser(s.userId)
+func (s *Session) GetUser() User {
+	user, _ := FindUserById(s.UserId)
+	return user
 }
 
-func (s *Session) Remove() {
+func (s *Session) Remove() error {
 
+	var err error
+	_, queryErr := app.DB.Exec(`
+		DELETE FROM Sessions WHERE id=?`, s.Id)
+
+	if queryErr != nil {
+		revel.INFO.Print("Error in removeSession: ", queryErr)
+		err = errors.New("DB error")
+	}
+
+	return err
 }
